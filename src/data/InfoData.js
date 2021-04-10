883a044b6594c2dc4cfc69c98fdd25e57a747e6c
Hamza Daqa <hamza.duqa@gmail.com>
2021-04-10 20:58:15 +0200

    react styled components

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from '../images/ImageOne.jpg'
+import ImageTwo from '../images/ImageTwo.jpg'
+
+export const InfoData = {
+    heading: 'Explore our beautiful homes',
+    paragraphOne: 'One of the hottest destinations in Costa Blanca, luxury homes situated in Campoamor, located near to the coast, golf course, and shopping center.',
+    paragraphTwo: 'Two of the hottest destinations in Costa Blanca, luxury homes situated in Campoamor, located near to the coast, golf course, and shopping center.',
+    buttonLabel: 'View Homes',
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+}
+
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragraphOne: ' in Costa Blanca, luxury homes situated in Campoamor, located near to the coast, golf course, and shopping center.',
+    paragraphTwo: ' Costa Blanca, luxury homes situated in Campoamor, located near to the coast, golf course, and shopping center.',
+    buttonLabel: 'View Homes',
+    image: ImageTwo,
+    reverse: true,
+    delay: 300
+}
